a71d4a0431c96de2c7d773e73e19b33cd379c8f7
Kristoffer Dalby <kradalby@kradalby.no>
2013-09-06 13:14:22 +0200

    fjernet duplikat filer

--- src/helperclasses/Attack.java
+++
@@ -1,8 +0,0 @@
-package helperclasses;
-
-public class Attack {
-	private Sheep sheep;;
-	private String latitude;
-	private String longitude;
-	private String timestamp; //burde endres til datetime
-}
