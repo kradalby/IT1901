d1ff971ae2aff6eb0f451cd9a2b121e18435b48e
Kristoffer Dalby <kradalby@kradalby.no>
2013-09-06 13:12:21 +0200

    satt opp maven og endrett strukturen

---
+++ sheepfarmer3000/src/main/java/org/prosjekt/helperclasses/Attack.java
@@ -0,0 +1,8 @@
+package helperclasses;
+
+public class Attack {
+	private Sheep sheep;;
+	private String latitude;
+	private String longitude;
+	private String timestamp; //burde endres til datetime
+}
