56482367738c9277ef6d931dedc559b50556663c
Kristoffer Dalby <kradalby@kradalby.no>
2013-09-09 11:05:07 +0200

    startet å teste socket funksjonalitet

--- sheepfarmer3000/src/main/java/org/prosjekt/server/Server.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/server/Server.java
@@ -1,5 +1,17 @@
-package server;
+package org.prosjekt.server;
 
 public class Server {
 
+	public int port;
+	public int maxCon;
+
+	public Server(int port, int maxCon) {
+		this.port = port;
+		this.maxCon = maxCon;
+	}
+	
+	public static void main(String args[]) throws IOException {
+		
+	}
+	
 }
