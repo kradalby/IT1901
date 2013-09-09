ed10c0795f0321780082827f8445f1722202627d
Kristoffer Dalby <kradalby@kradalby.no>
2013-09-09 11:17:29 +0200

    skrev litt kode for å teste java socket

--- sheepfarmer3000/src/main/java/org/prosjekt/server/Server.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/server/Server.java
@@ -1,5 +1,13 @@
 package org.prosjekt.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
 	public int port;
@@ -11,7 +19,23 @@ public class Server {
 	}
 	
 	public static void main(String args[]) throws IOException {
-		
+		System.out.println("merp derp lager server: ");
+		ServerSocket serverSocket = new ServerSocket(1234);
+		while(true) {
+			Socket socket = serverSocket.accept();
+			OutputStream os = socket.getOutputStream();
+			PrintWriter pw = new PrintWriter(os, true);
+			pw.println("merp?");
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String str = br.readLine();
+			
+			pw.println("Syre, " + str);
+			pw.close();
+			socket.close();
+			
+			System.out.println("Derp: " + str);
+		}
 	}
 	
 }
