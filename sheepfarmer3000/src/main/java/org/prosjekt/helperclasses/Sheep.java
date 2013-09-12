f27ede22152a23e7ed809767c5062002e4266ce7
Kristoffer Dalby <kradalby@kradalby.no>
2013-09-12 15:13:50 +0200

    endret birth til datetime object

--- sheepfarmer3000/src/main/java/org/prosjekt/helperclasses/Sheep.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/helperclasses/Sheep.java
@@ -2,18 +2,19 @@ package org.prosjekt.helperclasses;
 
 import java.util.ArrayList;
 
+import org.joda.time.DateTime;
+
 public class Sheep {
 	private final int id;
 	private boolean alive;
 	private int weight;
 	private String attack;
-	private String birth;
+	private DateTime birth;
 	private ArrayList<> cordinates;
 	private Farmer farmer;
 
-	public Sheep(int id, String name, Farmer farmer) {
+	public Sheep(int id, String name, Farmer farmer, int weight) {
 		this.id = id;
-		this.name = name;
 		this.farmer = farmer;
 	}
 }
