2d5b08f3c181553f1eef029eb2efc1e82e23c518
Kristoffer Dalby <kradalby@kradalby.no>
2013-09-12 15:18:14 +0200

    gjorde ferdig konstruktøren for sau per nå

--- sheepfarmer3000/src/main/java/org/prosjekt/helperclasses/Sheep.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/helperclasses/Sheep.java
@@ -9,12 +9,17 @@ public class Sheep {
 	private boolean alive;
 	private int weight;
 	private String attack;
-	private DateTime birth;
+	private final DateTime birth;
 	private ArrayList<> cordinates;
 	private Farmer farmer;
 
-	public Sheep(int id, String name, Farmer farmer, int weight) {
+	public Sheep(int id, int weight, DateTime birth, Farmer farmer) {
 		this.id = id;
+		this.weight = weight;
+		this.birth = birth;
 		this.farmer = farmer;
 	}
+
+
+
 }
