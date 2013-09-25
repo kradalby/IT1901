a25c3274494a0e1dcb161286b45b132a1ed2eb97
Kristoffer Dalby <kradalby@kradalby.no>
2013-09-25 18:41:11 +0200

    rettet skrivefeil i operation, la til getters og dokumenterte litt javadoc

--- sheepfarmer3000/src/main/java/org/prosjekt/helperclasses/NetPkg.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/helperclasses/NetPkg.java
@@ -3,9 +3,30 @@ package org.prosjekt.helperclasses;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * @author kradalby
+ * Dette er en klasse som sendes mellom klient og server. Objektene vi vil 
+ * sende(sheep, farmer samt login informasjon og annet) pakkes inn og legges 
+ * i ArrayListen data og pakkes opp og utfores ved hjelp av enumen operation.
+ * 
+ * @param <T> holder for objektene som skal plasseres der.
+ */
 public class NetPkg<T> implements Serializable {
 
-	NetPkgEnum operaton;
+	NetPkgEnum operation;
 	ArrayList<T> data;
+
+	public NetPkg(NetPkgEnum o, ArrayList<T> d) {
+		this.operation = o;
+		this.data = d;
+	}
+
+	public NetPkgEnum getOperation() {
+		return operation;
+	}
+
+	public ArrayList<T> getData() {
+		return data;
+	}
 	
 }
