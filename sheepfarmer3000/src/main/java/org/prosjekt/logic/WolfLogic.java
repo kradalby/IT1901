07406d451c607fe299e24264ec6cacc7da47ceb0
Kristoffer Dalby <kradalby@kradalby.no>
2013-09-25 14:27:14 +0200

    laget en getrandomsheep klasse og startet paa drep sauen klasse

---
+++ sheepfarmer3000/src/main/java/org/prosjekt/logic/WolfLogic.java
@@ -0,0 +1,24 @@
+package org.prosjekt.logic;
+
+import java.util.ArrayList;
+
+import org.prosjekt.helperclasses.Sheep;
+
+public class WolfLogic {
+
+	// Returnerer en vilkaarlig sau utifra en arraylist.
+	public static Sheep getRandomSheep(ArrayList<Sheep> sheeps) {
+		int n = sheeps.size();
+		int sheepId= 0 + (int)(Math.random() * (((n-1) - 0) + 1));
+		Sheep sheep = sheeps.get(sheepId);
+		return sheep;
+	}
+	
+	public static void wolfAttack(Sheep sheep) {
+		int chance = (int)(Math.random() * (((100-1) - 0) + 1));
+		if (chance > 30) {
+			sheep.setAlive(false);
+			
+		}
+	}
+}
