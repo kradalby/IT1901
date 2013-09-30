4729532568c5713907e00acd1e837aa88d6f0373
Kristoffer Dalby <kradalby@kradalby.no>
2013-09-30 12:01:23 +0200

    herp derp

--- sheepfarmer3000/src/main/java/org/prosjekt/client/ClientNetPkg.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/client/ClientNetPkg.java
@@ -1,5 +1,17 @@
 package org.prosjekt.client;
 
+import java.util.ArrayList;
+
+import org.prosjekt.helperclasses.NetPkg;
+import org.prosjekt.helperclasses.NetPkgEnum;
+
 public class ClientNetPkg {
 
+	NetPkg pkg;
+	NetPkgEnum operation;
+	ArrayList<T> data;
+	
+	
+	
+	
 }
