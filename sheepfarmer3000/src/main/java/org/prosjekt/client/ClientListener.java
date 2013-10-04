c09c48cadac1469614a7aea5220953aea2f10f2e
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-04 12:31:54 +0200

    startet aa skrive klient lytteren

--- sheepfarmer3000/src/main/java/org/prosjekt/client/ClientListener.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/client/ClientListener.java
@@ -1,5 +1,47 @@
 package org.prosjekt.client;
 
-public class ClientListener {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
+public class ClientListener implements Runnable {
+	
+	private ObjectOutputStream out = null;
+	private ObjectInputStream in = null;
+	private Socket listningSocket = null;
+	private int port;
+	private String host;
+	private boolean run = true;
+	
+	
+	public ClientListener(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+	
+	public void run() {
+		
+		try {
+			
+			listningSocket = new Socket(this.host, this.port);
+			in = new ObjectInputStream(listningSocket.getInputStream());
+			out = new ObjectOutputStream(listningSocket.getOutputStream());
+			
+			
+			while (run == true) {
+				
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	public void close() {
+		this.run = false;
+	}
+	
 }
