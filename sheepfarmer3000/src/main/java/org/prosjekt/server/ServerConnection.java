4caebb43d24dd3ef44261306bbeb2c6f8cc5a524
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-08 14:58:50 +0200

    skrev boilerplate server logic for aa starte en session

--- sheepfarmer3000/src/main/java/org/prosjekt/server/ServerConnection.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/server/ServerConnection.java
@@ -1,5 +1,33 @@
-package server;
+package org.prosjekt.server;
 
-public class ServerConnection {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class ServerConnection extends Thread{
+
+	private final int port = 4455;
+	private boolean running;
+	
+	public void run() {
+		running = true;
+		try {
+			ServerSocket serverSocket = new ServerSocket();
+			int sessionId = 0;
+			while(running) {
+				Socket socket;
+				socket = (Socket) serverSocket.accept();
+				
+				
+				sessionId++;
+				System.out.println("New server session started with id: " + sessionId);
+				ServerSession serverSession;
+				serverSession = new ServerSession(socket, sessionId);
+				
+			}
+		}
+	}
+	
+	public void close() {
+		running = false;
+	}
 }
