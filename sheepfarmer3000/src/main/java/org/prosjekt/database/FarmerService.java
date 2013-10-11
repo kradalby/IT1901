fba34ffc1c24f42d2c94326fd6a9afb49fc94197
Christoffer Buvik <christofferbuvik@gmail.com>
2013-10-11 13:08:04 +0200

    oppdaterte SheepService og FarmerService etter møte. Ble enige om hva vi trenger av kommunikasjon mellom server og client.

---
+++ sheepfarmer3000/src/main/java/org/prosjekt/database/FarmerService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.prosjekt.database;
+
+import java.util.List;
+import org.prosjekt.helperclasses.Coordinate;
+import org.prosjekt.helperclasses.Farmer;
+import org.prosjekt.helperclasses.Passhash;
+import org.prosjekt.helperclasses.Sheep;
+
+/**
+ *
+ * @author Christoffer <christofferbuvik@gmail.com>
+ */
+public interface FarmerService {
+    public void setPasshash(Passhash passhash);
+    public Passhash getPasshash(int farmerid);
+    
+    public void updateFarmer(Farmer farmer);
+    
+    /**
+     * 
+     * @param farmerid
+     * @return Farmer object with all sheeps, only last coordinate of each sheep.  
+     */
+    public Farmer getFarmer(int farmerid);
+    public void addSheep(int farmerid, Sheep sheep);
+}
---
+++ sheepfarmer3000/src/main/java/org/prosjekt/database/SheepService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.prosjekt.database;
+
+import org.prosjekt.helperclasses.Coordinate;
+import org.prosjekt.helperclasses.Sheep;
+
+/**
+ *
+ * @author Christoffer <christofferbuvik@gmail.com>
+ */
+public interface SheepService {
+    /**
+     * 
+     * @param sheep
+     * @return sheep with all coordinates 
+     */
+    public Sheep getSheepAllCordinates(Sheep sheep);
+    public void updateSheep();
+    
+    
+    
+}
