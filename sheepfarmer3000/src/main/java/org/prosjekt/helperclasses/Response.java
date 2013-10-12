d3d7979fde8bc943f12158ec715975ed714817da
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-12 15:11:36 +0200

    laget request og respons objekter som skal holde dataen som overfores.

--- sheepfarmer3000/src/main/java/org/prosjekt/helperclasses/Response.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/helperclasses/Response.java
@@ -1,5 +1,40 @@
 package org.prosjekt.helperclasses;
 
-public class Response {
+import java.io.Serializable;
+import java.util.HashMap;
 
+public class Response implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 11231235234L;
+	private Sheep sheep;
+	private Sheep[] sheeps;
+	private HashMap<String,Object> items = new HashMap<String,Object>();
+	
+	
+	public Sheep getSheep() {
+		return sheep;
+	}
+	public void setSheep(Sheep sheep) {
+		this.sheep = sheep;
+	}
+	public Sheep[] getSheeps() {
+		return sheeps;
+	}
+	public void addSheeps(Sheep[] sheeps) {
+		this.sheeps = sheeps;
+	}
+	
+	
+	public void addItem(String key, Object object) {
+		this.items.put(key, object);
+	}
+	
+	public Object getItem(String key) {
+		return this.items.get(key);
+	}
+	
+	
 }
