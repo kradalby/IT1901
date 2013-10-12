f1f1bc0c7f6d584b17b39838bb9e838ebae87e02
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-12 18:12:47 +0200

    laget skjelett for utforende enhet paa server siden

--- sheepfarmer3000/src/main/java/org/prosjekt/server/ServerWorker.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/server/ServerWorker.java
@@ -1,5 +1,45 @@
 package org.prosjekt.server;
 
-public class ServerWorker {
+import org.prosjekt.helperclasses.Request;
+import org.prosjekt.helperclasses.Response;
 
+public class ServerWorker {
+	
+	public static Response handlePackage(Request request) {
+		Response response = new Response();
+		
+		if (request == null) {
+			response.addItem("error", "Request was null");
+		} else {
+			switch (request.getCommand()) {
+			case LOGIN:
+				//loginmethod
+				break;
+			case GETSHEEPBYID:
+				//getonesheep
+				break;
+			case GETALLSHEEPS:
+				break;
+			case GETALLUSERSSHEEPS:
+				break;
+			case GETALLUSERSSHEEPSALIVE:
+				break;
+			case GETALLUSERSSHEEPSDEAD:
+				break;
+			case ALTERSHEEP:
+				break;
+			case ALTERUSER:
+				break;
+			case GETUSERINFO:
+				break;
+			case ADDSHEEP:
+				break;
+			default:
+				response.addItem("error", "Unknown request");
+				break;
+			}
+		}
+		
+		return response;
+	}
 }
