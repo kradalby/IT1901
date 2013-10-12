c77165410d723f49582c36ad595284fa471033a8
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-12 13:13:34 +0200

    laget run funksjonen

--- sheepfarmer3000/src/main/java/org/prosjekt/server/ServerSession.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/server/ServerSession.java
@@ -4,29 +4,43 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import org.prosjekt.helperclasses.Request;
+
 public class ServerSession extends Thread {
 
 	private Socket socket;
 	private int id;
+	private boolean running = true;
 	
 	public ServerSession(Socket socket, int id) {
 		this.socket = socket;
 		this.id = id;
-		
 	}
 	
 	ObjectOutputStream out;
 	ObjectInputStream in;
 	
 	public void run() {
+		try {
+			System.out.println("New server connection opened.");
+			System.out.println("Session opened with ID: " + id);
+			
+			while(running) {
+				Request request = ServerWorker.doshiat(reciveObject()); //Doshiat er en placeholder til en ordentligmetode blir laget.
+				sendObject(request);
+			}
+			
+			System.out.println("Connection with session id: " + id + " closed.");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Request reciveObject() {
 		
 	}
 	
-	public void recieveObject() {
-		
-	}
-	
-	public void sendObject() {
+	public boolean sendObject() {
 		
 	}
 	
