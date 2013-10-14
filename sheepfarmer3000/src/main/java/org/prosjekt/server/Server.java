ae387377731523bb18f5d0487de149a7fb482558
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-14 13:37:23 +0200

    fikset main funksjonen

--- sheepfarmer3000/src/main/java/org/prosjekt/server/Server.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/server/Server.java
@@ -3,7 +3,7 @@ package org.prosjekt.server;
 public class Server {
 
 	
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		ServerConnection serverConnection;
 		serverConnection = new ServerConnection();
 		serverConnection.start();
