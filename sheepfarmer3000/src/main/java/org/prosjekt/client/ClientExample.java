7dde2fc731ed8a9db33ee57f81263690b31c1e46
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-14 13:39:26 +0200

    work in progress client eksempel klasse, paa hvordan man sporr servern om ting

---
+++ sheepfarmer3000/src/main/java/org/prosjekt/client/ClientExample.java
@@ -0,0 +1,33 @@
+package org.prosjekt.client;
+
+import org.prosjekt.helperclasses.Request;
+import org.prosjekt.helperclasses.RequestEnum;
+import org.prosjekt.helperclasses.Response;
+import org.prosjekt.helperclasses.Sheep;
+
+public class ClientExample {
+
+	static ClientConnection connection = new ClientConnection("localhost", 4455);
+	static boolean connected = connection.openServerConnection();
+	
+	public static void main(String[] args) {
+		Sheep[] sheeps = getAllSheeps();
+		for (int i = 0; i < sheeps.length; i++) {
+			System.out.println(sheeps[i].getId());
+		}
+	}
+	
+	public static Sheep[] getAllSheeps() {
+		if (connected) {
+			Request request = new Request();
+			request.setCommand(RequestEnum.GETALLSHEEPS);
+			connection.sendPackage(request);
+			Response response = (Response) connection.receivePackage();
+			return response.getSheeps();
+		} else {
+			return null;
+		}
+		
+	}
+}
+
