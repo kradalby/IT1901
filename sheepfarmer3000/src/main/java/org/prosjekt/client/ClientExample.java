0af66b78800fcaabb6da2cb01d41c39699fe25fc
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-19 12:22:45 +0200

    testet å sende ut sms

--- sheepfarmer3000/src/main/java/org/prosjekt/client/ClientExample.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/client/ClientExample.java
@@ -5,11 +5,12 @@ import org.prosjekt.helperclasses.Request;
 import org.prosjekt.helperclasses.RequestEnum;
 import org.prosjekt.helperclasses.Response;
 import org.prosjekt.helperclasses.Sheep;
+import org.prosjekt.helperclasses.Sms;
 
 public class ClientExample {
 
-	static ClientConnection connection = new ClientConnection("127.0.0.1", 4455);
-	static boolean connected = connection.openServerConnection();
+//	static ClientConnection connection = new ClientConnection("127.0.0.1", 4455);
+//	static boolean connected = connection.openServerConnection();
 	
 	public static void main(String[] args) {
 //		Sheep[] sheeps = getAllSheeps();
@@ -19,19 +20,22 @@ public class ClientExample {
 		
 		Mail mail = new Mail("kradalby@kradalby.no", "test", "dette er en test lol");
 		mail.sendMail();
+		
+		Sms sms = new Sms("4745673429", "Dette er en sauetest!");
+		sms.sendSMS();
 	}
 	
-	public static Sheep[] getAllSheeps() {
-		if (connected) {
-			Request request = new Request();
-			request.setCommand(RequestEnum.GETALLSHEEPS);
-			connection.sendPackage(request);
-			Response response = (Response) connection.receivePackage();
-			return response.getSheeps();
-		} else {
-			return null;
-		}
-		
-	}
+//	public static Sheep[] getAllSheeps() {
+//		if (connected) {
+//			Request request = new Request();
+//			request.setCommand(RequestEnum.GETALLSHEEPS);
+//			connection.sendPackage(request);
+//			Response response = (Response) connection.receivePackage();
+//			return response.getSheeps();
+//		} else {
+//			return null;
+//		}
+//		
+//	}
 }
 
