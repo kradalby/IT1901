7303870a399c7bfa321d712c8301dd65b93bd57d
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-22 16:12:59 +0200

    la til getallsheepsfunksjon for aa hente ut ALLE sauer i systemet

--- sheepfarmer3000/src/main/java/org/prosjekt/logic/SheepLogic.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/logic/SheepLogic.java
@@ -2,10 +2,18 @@ package org.prosjekt.logic;
 
 import java.util.ArrayList;
 
+import javax.net.ssl.SSLContext;
+
+import org.prosjekt.database.SheepService;
+import org.prosjekt.database.repository.LogicService;
+import org.prosjekt.database.repository.SheepRepository;
+import org.prosjekt.helperclasses.Coordinate;
 import org.prosjekt.helperclasses.Sheep;
+
 
 public class SheepLogic {
 	
+
 //	public static void moveSheeps(ArrayList<Sheep> sheeps) {
 //		final Runnable saueFlytter = new Runnable() { //metoden som skal gjentas
 //                public void run() { 
@@ -31,6 +39,21 @@ public class SheepLogic {
 //	}
 	
 	public static void moveSheeps() {
+		SheepService ss = new SheepRepository();
+		LogicService ls = new LogicRepository();
 		
+		Sheep[] sheeps = (Sheep[]) ss.getAllSheeps();
+		for (int i = 0; i < sheeps.length; i++) {
+			double currentLong = sheeps[i].getCurrentCordinate().getLongitude();
+			double currentLat = sheeps[i].getCurrentCordinate().getLatitude();
+			double newLong = currentLong + Math.random()/1000;
+			double newLat = currentLat + Math.random()/1000;
+			Coordinate newCoord = new Coordinate(newLong, newLat);
+			
+			//Dette er to metoder je gikke er sikker paa hva som er forskjellen paa
+			//Anyone?
+			sheeps[i].addCoordinate(newCoord);
+			ls.addSheepMovement(sheeps[i], newCoord);
+		}
 	}
 }
