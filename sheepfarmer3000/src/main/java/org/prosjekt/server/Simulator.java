bee5e7f613cef291654944639e015a901350a6bf
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-23 17:10:25 +0200

    skrev noe simuleringskode for aa flytte og drepe sauer

--- sheepfarmer3000/src/main/java/org/prosjekt/server/Simulator.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/server/Simulator.java
@@ -4,23 +4,55 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.prosjekt.database.SheepService;
+import org.prosjekt.database.repository.SheepRepository;
 import org.prosjekt.helperclasses.Farmer;
+import org.prosjekt.logic.SheepLogic;
+import org.prosjekt.logic.WolfLogic;
 
-public class Simulator extends Thread{
+public class Simulator extends Thread {
 	
-	private int numberOfSheeps;
-	private LinkedList<Coordinate> area = new LinkedList<Coordinate>();
-	private ArrayList<Farmer> farmers = new ArrayList<Farmer>();
+	private boolean running;
+	SheepService ss = new SheepRepository();
+	private int timeBetweenMoves = 600000;
+	private int timeBetweenAttacks = 172800000;
 	
-	
-	public Simulator(int numberOfSheeps, LinkedList<Coordinate> area, 
-			ArrayList<Farmer> farmers){
-		this.numberOfSheeps = numberOfSheeps;
-		this.area = area;
-		this.farmers = farmers;
+	public void run() {
+		running = true;
+		Move move = new Move();
+		move.start();
+		Kill killer = new Kill();
+		killer.start();
 	}
 	
-	public void generateTestData() {
+	public class Move extends Thread {
 		
+		public void run() {
+			while (running) {
+				try {
+					SheepLogic.moveSheeps();
+					Thread.sleep(timeBetweenMoves);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
+	
+	public class Kill extends Thread {
+		
+		public void run() {
+			while (running) {
+				try {
+					WolfLogic.wolfAttack(WolfLogic.getRandomSheep(ss.getAllSheeps()));
+					Thread.sleep(timeBetweenAttacks);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
 }
+
