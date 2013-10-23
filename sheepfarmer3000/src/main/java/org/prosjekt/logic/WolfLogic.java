cce5cdb43110fcaa2befa14aa11690f12766f6a6
Kristoffer Dalby <kradalby@kradalby.no>
2013-10-23 17:09:30 +0200

    endrett getrandomsheep til aa ta inn array istede for arraylist

--- sheepfarmer3000/src/main/java/org/prosjekt/logic/WolfLogic.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/logic/WolfLogic.java
@@ -12,18 +12,23 @@ import org.prosjekt.helperclasses.Sheep;
  */
 public class WolfLogic {
 
-	// Returnerer en vilkaarlig sau utifra en arraylist.
 	/**
+	 * Returnerer en vilkaarlig sau utifra en arraylist.
+	 * 
 	 * @param sheeps liste over alle sauer som kan angripes.
 	 * @return returnerer en tilfeldig sau.
 	 */
-	public static Sheep getRandomSheep(ArrayList<Sheep> sheeps) {
-		int n = sheeps.size();
+	public static Sheep getRandomSheep(Sheep[] sheeps) {
+		int n = sheeps.length;
 		int sheepId= 0 + (int)(Math.random() * (((n-1) - 0) + 1));
-		Sheep sheep = sheeps.get(sheepId);
+		Sheep sheep = sheeps[sheepId];
 		return sheep;
 	}
 	
+	/**
+	 * Tar inn en sau og dreper den 70% av gangene:)
+	 * @param sheep
+	 */
 	public static void wolfAttack(Sheep sheep) {
 		int chance = (int)(Math.random() * (((100-1) - 0) + 1));
 		
