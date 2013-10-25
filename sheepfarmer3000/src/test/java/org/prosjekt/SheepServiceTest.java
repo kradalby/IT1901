85815b9ac59ad19ab07ed7a5206c0188de2ff20e
Christoffer Buvik <christofferbuvik@gmail.com>
2013-10-25 11:43:22 +0200

    La til en test for hvert interface som server og logic modulene bruker for å kommunisere med databasen.

---
+++ sheepfarmer3000/src/test/java/org/prosjekt/SheepServiceTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.prosjekt;
+
+/**
+ *
+ * @author Christoffer <christofferbuvik@gmail.com>
+ */
+public class SheepServiceTest {
+    
+}
