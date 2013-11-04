033f7ed0d30b8f8fd185a8dc6086ec37abad8fea
Alfredvc <erpa_1913@hotmail.com>
2013-11-04 13:27:06 +0100

    lagde AttackMarker classe, endret SheepMarker

---
+++ sheepfarmer3000/src/main/java/org/prosjekt/dynamicmaps/AttackMarker.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.prosjekt.dynamicmaps;
+
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import org.prosjekt.helperclasses.Coordinate;
+
+
+/**
+ *
+ * @author Alfredo
+ */
+public class AttackMarker extends CustomMapMarker{
+    
+    public AttackMarker(Coordinate coord, String id){
+        super();
+         fileLocation = "C:\\Users\\Alfredo\\Documents\\NetBeansProjects\\New Folder\\TestProject\\crossmark.png";
+        try{
+            img = ImageIO.read(new File(fileLocation));
+        }
+        catch(IOException e){
+            img = null;
+        }
+        this.lon = coord.getLon();
+        this.lat = coord.getLat();
+        this.id = id;
+    }
+}
