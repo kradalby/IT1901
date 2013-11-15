f729031fe9fbf8656e9de722d4a8418da238ffdf
Christoffer Buvik <christofferbuvik@gmail.com>
2013-11-15 15:56:04 +0100

    bugfix i AttackCoordinate, dato ble feil i gui

--- sheepfarmer3000/src/main/java/org/prosjekt/gui/AttackCoordinate.java
+++ sheepfarmer3000/src/main/java/org/prosjekt/gui/AttackCoordinate.java
@@ -5,7 +5,7 @@ import org.prosjekt.helperclasses.Coordinate;
 public class AttackCoordinate extends Coordinate {
 	private String sheepId;
 	public AttackCoordinate(Coordinate coordinate, String sheepId){
-		super(coordinate.getLon(), coordinate.getLat());
+		super(coordinate.getLon(), coordinate.getLat(), coordinate.getDate());
 		this.sheepId = sheepId;
 	}
 	public String getSheepId(){
